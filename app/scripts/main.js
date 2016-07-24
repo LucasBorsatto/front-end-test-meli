679982e254b4f8930f1de9b91597327469fb43d7
LucasBorsatto <borsattolucas@gmail.com>
2016-07-24 11:50:57 -0300

    Update html (product description) and added Carousel. Added Tiny.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('\'Allo \'Allo!');
+(function (window) {
+    'use strict';
+
+    function qS(selector) { return document.querySelector(selector); };
+
+    var carouselOptions = {
+        'limitPerPage': 3,
+    };
+
+    // Create a new carousel with option
+    var carousel = new ch.Carousel(qS('.ch-carousel'), carouselOptions);
+
+}(this));

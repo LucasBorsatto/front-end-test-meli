e131fec2bc5481b2b9b9f5b78374a3ace5565b57
LucasBorsatto <borsattolucas@gmail.com>
2016-07-24 19:36:26 -0300

    Fixed Tooltip

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -15,7 +15,8 @@
     // Tooltip example component with options
     var tooltipOptions = {
         'side': 'bottom',
-        'align': 'left'
+        'align': 'left',
+        'content': 'Apple Ipad Mini 4 16gb 4g<p class=\'price\'><span class=\'ch-price\'>$ 6.799<sup>99</sup></span></p><span class=\'installment\'>6x <span class=\'ch-price\'>$ 1.300<sup>00</sup></span></span>'
     };
     var tooltip = new ch.Tooltip(qS('.tooltip img'), tooltipOptions);
 
